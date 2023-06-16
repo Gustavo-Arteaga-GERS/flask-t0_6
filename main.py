44a5b7adee44fc5c578480b6c844a8d259a0919e
Gustavo Arteaga <82400391+Gustavo-Arteaga-GERS@users.noreply.github.com>
2023-06-16 16:40:30 -0500

    add route "rate"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template ,request, make_response,jsonify
 from pulp import LpVariable, LpProblem, lpSum, value, LpMinimize
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -69,6 +71,73 @@ def sudoku():
 
   res = make_response(jsonify(solution), 200)
   return res
+
+
+
+@app.route("/rate", methods = ['POST'])
+def rate():
+    
+    # Define the URL of the website you want to scrape
+    
+    url = "https://www.superfinanciera.gov.co/jsp/"
+    # url = "https://www.banrep.gov.co/docum/buscador_series.html"
+    
+    # Send a GET request to the website
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    
+    # response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
+        #print("*******************")
+        divs = soup.find_all("div", class_="cont_Indicador")
+    
+        print("*******************")
+        string_tmr = divs[0].text
+        tmr = string_tmr[-8:]
+        print(tmr)
+        print("*******************")
+    
+    else:
+        print("Failed to retrieve the web page TMR. ☹️")
+    
+    
+    ## EUR / USD
+    url2 = "https://www.xe.com/es/currencyconverter/convert/?Amount=1&From=EUR&To=USD"
+    # Send a GET request to the website
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
+    response = requests.get(url2, headers=headers)
+    
+    
+    
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
+        # print("*******************")
+        all_p = soup.find_all("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod")
+        
+        string_rate = all_p[0].text
+        rate = string_rate[:4]
+        print("*******************")
+        print(rate)
+        print("*******************")
+           
+    else:
+        print("Failed to retrieve the web page EUR-USD. ☹️")
+    
+    
+    solution = {
+        value_TMR:tmr,
+        value_rate: rate
+    }
+    
+    res = make_response(jsonify(solution), 200)
+    return res
 
 if __name__ == '__main__':
   app.run(port=5000)
